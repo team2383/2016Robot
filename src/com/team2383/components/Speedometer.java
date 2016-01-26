7798bb6b19875d0ab2f5dcfce096dd2724a8bb80
Matthew Alonso <=>
2016-01-26 17:35:18 -0500

    refactor code

--- src/com/team2383/components/Speedometer.java
+++ src/com/team2383/components/Speedometer.java
@@ -1,17 +1,17 @@
 package com.team2383.components;
 
 import java.util.function.DoubleSupplier;
-import java.util.function.DoublePredicate;
 
 import org.strongback.annotation.ThreadSafe;
+import org.strongback.components.DistanceSensor;
 import org.strongback.components.Gyroscope;
 import org.strongback.components.SpeedSensor;
 import org.strongback.function.DoubleToDoubleFunction;
 
-
 /**
- * A speedometer is a device that measures the instantaneous linear speed of a device in inches per second. 
- * Convenience constructors are provided to convert one or more {@link Gyroscope} to a {@link Speedometer}.
+ * A speedometer is a device that measures the instantaneous linear speed of a
+ * device in inches per second. Convenience constructors are provided to convert
+ * one or more {@link Gyroscope} to a {@link Speedometer}.
  *
  * @author Matthew Alonso
  *
@@ -21,56 +21,58 @@ import org.strongback.function.DoubleToDoubleFunction;
 public interface Speedometer extends SpeedSensor {
 	/**
 	 * Gets the current speed of this {@link Speedometer} in in/s
-	 * 
+	 *
 	 * @return the value of this {@link Speedometer}
 	 * @see #getSpeedInFeetPerSecond()
 	 */
 	public double getSpeedInInchesPerSecond();
-	
+
 	/**
-     * Gets the current value of this {@link DistanceSensor} in ft/s.
-     *
-     * @return the value of this {@link DistanceSensor}
-     * @see #getDistanceInInches()
-     */
+	 * Gets the current value of this {@link DistanceSensor} in ft/s.
+	 *
+	 * @return the value of this {@link DistanceSensor}
+	 * @see #getDistanceInInches()
+	 */
 	default public double getSpeedInFeetPerSecond() {
 		return getSpeedInInchesPerSecond() / 12.0;
 	};
-	
+
 	/**
-	 * Gets the current speed of this {@link Speedometer}.
-	 * Default unit is ft/s
-	 * 
+	 * Gets the current speed of this {@link Speedometer}. Default unit is ft/s
+	 *
 	 * @return the value of this {@link Speedometer}
 	 * @see #getSpeedInFeetPerSecond()
 	 */
+	@Override
 	default public double getSpeed() {
 		return getSpeedInFeetPerSecond();
 	}
-    
-    /**
-     * Create a speedometer for the given functions that return angular velocity, and a function to convert
-     * angular velocity into linear speed in in/s.
-     *
-     * @param scale constant scaling factor that turns the angular velocity to linear speed in inches per second.
-     * @param angularVelocity the function(s) that returns the angular velocity; may not be null
-     * @return the speedometer
-     */
-    public static Speedometer create(DoubleToDoubleFunction scaleFunction, DoubleSupplier... angularVelocitySuppliers) {
-        return new Speedometer() {
-            @Override
-            public double getSpeedInInchesPerSecond() {
-            	double speed = 0;
-            	
-            	for (DoubleSupplier angularVelocitySupplier : angularVelocitySuppliers) {
-            		speed += angularVelocitySupplier.getAsDouble();
-            	}
-            	
-            	speed /= angularVelocitySuppliers.length;
-            	speed = scaleFunction.applyAsDouble(speed);
-            	
-            	return speed;
-            }
-        };
-    }
+
+	/**
+	 * Create a speedometer for the given functions that return angular
+	 * velocity, and a function to convert angular velocity into linear speed in
+	 * in/s.
+	 *
+	 * @param scale
+	 *            constant scaling factor that turns the angular velocity to
+	 *            linear speed in inches per second.
+	 * @param angularVelocity
+	 *            the function(s) that returns the angular velocity; may not be
+	 *            null
+	 * @return the speedometer
+	 */
+	public static Speedometer create(DoubleToDoubleFunction scaleFunction, DoubleSupplier... angularVelocitySuppliers) {
+		return () -> {
+			double speed = 0;
+
+			for (final DoubleSupplier angularVelocitySupplier : angularVelocitySuppliers) {
+				speed += angularVelocitySupplier.getAsDouble();
+			}
+
+			speed /= angularVelocitySuppliers.length;
+			speed = scaleFunction.applyAsDouble(speed);
+
+			return speed;
+		};
+	}
 }
--- src/com/team2383/robot/Robot.java
+++ src/com/team2383/robot/Robot.java
@@ -4,140 +4,164 @@ package com.team2383.robot;
 import org.strongback.Strongback;
 import org.strongback.SwitchReactor;
 import org.strongback.components.Motor;
-import org.strongback.components.TalonSRX;
-import org.strongback.hardware.Hardware;
-
-import com.team2383.command.*;
-import com.team2383.components.Speedometer;
-
-import org.strongback.drive.TankDrive;
-import org.strongback.components.ui.ContinuousRange;
-import org.strongback.components.ui.FlightStick;
 import org.strongback.components.Solenoid;
 import org.strongback.components.Switch;
+import org.strongback.components.TalonSRX;
+import org.strongback.components.ui.ContinuousRange;
+import org.strongback.components.ui.FlightStick;
+import org.strongback.drive.TankDrive;
+import org.strongback.hardware.Hardware;
 
-import edu.wpi.first.wpilibj.Timer;
-import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
-import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import com.team2383.components.Speedometer;
+
 import edu.wpi.first.wpilibj.CANTalon;
 import edu.wpi.first.wpilibj.CANTalon.FeedbackDevice;
 import edu.wpi.first.wpilibj.IterativeRobot;
+import edu.wpi.first.wpilibj.Timer;
+import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
 public class Robot extends IterativeRobot {
 
-    private static TankDrive drive;
-    private static FlightStick leftJoystick;
-    private static FlightStick rightJoystick;
-    private static FlightStick operatorJoystick;
-    private static ContinuousRange leftSpeed;
-    private static ContinuousRange rightSpeed;
-    private static Speedometer speedometer;
-    
-    private final String defaultAuto = "Default Auto";
-    private final String secondAuto = "Second Auto";
-    private String autoSelected;
-    private SendableChooser chooser;
-    
-    
-    private static Solenoid leftSolenoidShifter;
-    private static Solenoid rightSolenoidShifter;
-    //switch reactors
-    private static SwitchReactor reactor = Strongback.switchReactor();
+	private final String defaultAuto = "Default Auto";
+	private final String secondAuto = "Second Auto";
+	private String autoSelected;
 
-    @Override
-    public void robotInit() {
-        // Set up Strongback using its configurator. This is entirely optional, but we're not using
-        // events or data so it's better if we turn them off. All other defaults are fine.
-        Strongback.configure().recordNoEvents().recordNoData().initialize();
-        
-        reactor = Strongback.switchReactor();
-        
-        
-        CANTalon leftFront_WPI = new CANTalon(Config.LEFT_FRONT_MOTOR_PORT);
-        CANTalon rightRear_WPI = new CANTalon(Config.RIGHT_REAR_MOTOR_PORT);
-       
-        TalonSRX leftFront = Hardware.Motors.talonSRX(leftFront_WPI);
-        TalonSRX rightRear = Hardware.Motors.talonSRX(rightRear_WPI);
+	private static SendableChooser chooser;
 
-        leftFront_WPI.setFeedbackDevice(FeedbackDevice.CtreMagEncoder_Relative);
-        rightRear_WPI.setFeedbackDevice(FeedbackDevice.CtreMagEncoder_Relative);
-        
-        TalonSRX leftRear = Hardware.Motors.talonSRX(Config.LEFT_REAR_MOTOR_PORT);
-        TalonSRX rightFront = Hardware.Motors.talonSRX(Config.RIGHT_FRONT_MOTOR_PORT);
-    
-        Motor left = Motor.compose(leftFront, leftRear);
-        Motor right = Motor.compose(rightFront, rightRear).invert();
-        
-        leftSolenoidShifter= Hardware.Solenoids.doubleSolenoid(Config.LEFT_EXTEND_SHIFTER_PORT,
-        		 									  Config.LEFT_RETRACT_SHIFTER_PORT,
-        		 									  Solenoid.Direction.STOPPED);
-        rightSolenoidShifter= Hardware.Solenoids.doubleSolenoid(Config.RIGHT_EXTEND_SHIFTER_PORT,
-        		 									  Config.RIGHT_RETRACT_SHIFTER_PORT,
-        		 									  Solenoid.Direction.STOPPED);
-        
-        
-        speedometer = Speedometer.create(speed -> {
-        	//revolution/sec to in/sec
-        	speed *= Config.WHEEL_CIRCUMFERENCE;
-        	
-        	SmartDashboard.putNumber("s", speed/12.0);
-        	return speed;
-        	
-        }, leftFront_WPI::getPulseWidthVelocity,
-           rightRear_WPI::getPulseWidthVelocity);
+	@Override
+	public void robotInit() {
+		// Set up Strongback using its configurator. This is entirely optional,
+		// but we're not using
+		// events or data so it's better if we turn them off. All other defaults
+		// are fine.
+		Strongback.configure().recordNoEvents().recordNoData().initialize();
 
-        drive = new TankDrive(left, right);
-        leftJoystick = Hardware.HumanInterfaceDevices.logitechAttack3D(Config.LEFT_JOYSTICK_PORT);
-        rightJoystick = Hardware.HumanInterfaceDevices.logitechAttack3D(Config.RIGHT_JOYSTICK_PORT);
-        operatorJoystick = Hardware.HumanInterfaceDevices.logitechAttack3D(Config.OPERATOR_JOYSTICK_PORT);
-        leftSpeed = leftJoystick.getYaw();
-        rightSpeed = rightJoystick.getYaw(); 
-        
-        chooser = new SendableChooser();
-        chooser.addDefault("Default Auto", defaultAuto);
-        chooser.addObject("Second Auto", secondAuto);	
-        SmartDashboard.putData("Auto Choices", chooser);
-        
-    }
+		CANTalon leftFront_WPI = new CANTalon(Config.LEFT_FRONT_MOTOR_PORT);
+		CANTalon rightRear_WPI = new CANTalon(Config.RIGHT_REAR_MOTOR_PORT);
 
-    public void autonomousInit(){
-    	autoSelected = (String) chooser.getSelected();
-    	System.out.println("Auto Selected: " + autoSelected);
-    }
-    
-    public void autonomousPeriodic(){
-    	switch(autoSelected){
-    	case secondAuto:
-    		//secondAuto command here
-    		break;
-    	case defaultAuto:
-    		//defaultAuto command here
-    		break; 
-    	}
-    }
-    
-    @Override
-    public void teleopInit() {
-        // Start Strongback functions ...
-        Strongback.restart();
+		TalonSRX leftFront = Hardware.Motors.talonSRX(leftFront_WPI);
+		TalonSRX rightRear = Hardware.Motors.talonSRX(rightRear_WPI);
 
-        drive.tank(leftSpeed.read(), rightSpeed.read());
-    }
+		leftFront_WPI.setFeedbackDevice(FeedbackDevice.CtreMagEncoder_Relative);
+		rightRear_WPI.setFeedbackDevice(FeedbackDevice.CtreMagEncoder_Relative);
 
-    @Override
-    public void teleopPeriodic() {
-    	//Strongback.submit(new GamepadDriveConsole(drive, speedometer, Hardware.HumanInterfaceDevices.logitechF310(0)));
-    	Strongback.submit(new TwinJoystickDriveConsole(drive, speedometer, Hardware.HumanInterfaceDevices.logitechAttack3D(0)));
-    }
-    
-    
-    @Override
-    public void disabledInit() {
-        // Tell Strongback that the robot is disabled so it can flush and kill commands.
-        Strongback.disable();   
-    }
-    
-    @Override
-    public void disabledPeriodic() {}
+		TalonSRX leftRear = Hardware.Motors.talonSRX(Config.LEFT_REAR_MOTOR_PORT);
+		TalonSRX rightFront = Hardware.Motors.talonSRX(Config.RIGHT_FRONT_MOTOR_PORT);
+
+		Motor left = Motor.compose(leftFront, leftRear);
+		Motor right = Motor.compose(rightFront, rightRear).invert();
+
+		Motor feeder = Hardware.Motors.talon(Config.FEEDER_MOTOR_PORT);
+		Motor climberPivot = Hardware.Motors.talon(Config.CLIMBER_MOTOR_PORT);
+
+		Solenoid leftSolenoidShifter = Hardware.Solenoids.doubleSolenoid(Config.LEFT_EXTEND_SHIFTER_PORT,
+				Config.LEFT_RETRACT_SHIFTER_PORT, Solenoid.Direction.STOPPED);
+		Solenoid rightSolenoidShifter = Hardware.Solenoids.doubleSolenoid(Config.RIGHT_EXTEND_SHIFTER_PORT,
+				Config.RIGHT_RETRACT_SHIFTER_PORT, Solenoid.Direction.STOPPED);
+
+		Speedometer speedometer = Speedometer.create(speed -> {
+			// revolution/sec to in/sec
+			speed *= Config.WHEEL_CIRCUMFERENCE;
+
+			SmartDashboard.putNumber("s", speed / 12.0);
+			return speed;
+
+		} , leftFront_WPI::getPulseWidthVelocity, rightRear_WPI::getPulseWidthVelocity);
+
+		TankDrive drive = new TankDrive(left, right);
+
+		FlightStick leftJoystick = Hardware.HumanInterfaceDevices.logitechAttack3D(Config.LEFT_JOYSTICK_PORT);
+		FlightStick rightJoystick = Hardware.HumanInterfaceDevices.logitechAttack3D(Config.RIGHT_JOYSTICK_PORT);
+		FlightStick operatorJoystick = Hardware.HumanInterfaceDevices.logitechAttack3D(Config.OPERATOR_JOYSTICK_PORT);
+		ContinuousRange leftSpeed = leftJoystick.getPitch();
+		ContinuousRange rightSpeed = rightJoystick.getPitch();
+
+		chooser = new SendableChooser();
+		chooser.addDefault("Default Auto", defaultAuto);
+		chooser.addObject("Second Auto", secondAuto);
+		SmartDashboard.putData("Auto Choices", chooser);
+
+		// joystick buttons
+		Switch shiftUp = rightJoystick.getButton(3);
+		Switch shiftDown = rightJoystick.getButton(4);
+		Switch feedIn = leftJoystick.getButton(5);
+		Switch feedOut = leftJoystick.getButton(6);
+		Switch climberUp = operatorJoystick.getButton(4);
+		Switch climberDown = operatorJoystick.getButton(5);
+
+		// todo: add doubletap to strongback OI
+		Switch climberExtend = operatorJoystick.getButton(6);
+
+		SwitchReactor reactor = Strongback.switchReactor();
+
+		// shifter
+		reactor.onTriggered(shiftUp, () -> {
+			leftSolenoidShifter.extend();
+			rightSolenoidShifter.extend();
+		});
+		reactor.onTriggered(shiftDown, () -> {
+			leftSolenoidShifter.retract();
+			rightSolenoidShifter.retract();
+		});
+
+		// feeder
+		reactor.whileTriggered(feedIn, () -> {
+			feeder.setSpeed(0.5);
+		});
+		reactor.whileTriggered(feedOut, () -> {
+			feeder.setSpeed(-0.5);
+		});
+
+		// climber
+		reactor.whileTriggered(climberUp, () -> {
+			climberPivot.setSpeed(1);
+		});
+		reactor.whileTriggered(climberDown, () -> {
+			climberPivot.setSpeed(-1);
+		});
+
+		// drive
+		reactor.whileTriggered(Switch.alwaysTriggered(), () -> drive.tank(leftSpeed.read(), rightSpeed.read()));
+	}
+
+	@Override
+	public void autonomousInit() {
+		autoSelected = (String) chooser.getSelected();
+		System.out.println("Auto Selected: " + autoSelected);
+	}
+
+	@Override
+	public void autonomousPeriodic() {
+		switch (autoSelected) {
+		case secondAuto:
+			// secondAuto command here
+			break;
+		case defaultAuto:
+			// defaultAuto command here
+			break;
+		}
+	}
+
+	@Override
+	public void teleopInit() {
+		// Start Strongback functions ...
+		Strongback.restart();
+	}
+
+	@Override
+	public void teleopPeriodic() {
+		Timer.delay(0.005);
+	}
+
+	@Override
+	public void disabledInit() {
+		// Tell Strongback that the robot is disabled so it can flush and kill
+		// commands.
+		Strongback.disable();
+	}
+
+	@Override
+	public void disabledPeriodic() {
+	}
 }
