0e0bbe46da65dde226ee49302369da4e459ba745
Matthew Alonso <=>
2016-04-01 00:46:20 -0400

    Use the Vision Value with the Gyro instead of directly using the camera

--- src/com/team2383/robot/commands/VisionTurn.java
+++ src/com/team2383/robot/commands/VisionTurn.java
@@ -1,73 +1,27 @@
 package com.team2383.robot.commands;
 
-import static com.team2383.robot.HAL.drivetrain;
 import static com.team2383.robot.HAL.vision;
 
 import com.team2383.robot.Constants;
-import com.team2383.robot.subsystems.Drivetrain.Gear;
 
-import edu.wpi.first.wpilibj.command.PIDCommand;
-import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
-
-public class VisionTurn extends PIDCommand {
-
-	private double timeAtSetpoint;
-	private double lastCheck;
+public class VisionTurn extends GyroTurn {
 	private double timeWithoutTarget;
 
 	public VisionTurn() {
-		super("VisionTurn", Constants.visionTurnP, Constants.visionTurnI, Constants.visionTurnD, .01);
-		requires(drivetrain);
-		requires(vision); // since PID loop runs faster, we require vision to
-							// get
-							// faster updates in the PID loop
-		this.getPIDController().setInputRange(-60.0, 60.0);
-		this.getPIDController().setOutputRange(-0.7, 0.7);
-		this.getPIDController().setSetpoint(0);
-		SmartDashboard.putData("VisionTurn Controller", this.getPIDController());
+		super(0);
 	}
 
 	@Override
 	protected void initialize() {
-		drivetrain.enableBrake();
-		drivetrain.shiftTo(Gear.LOW);
+		super.initialize();
+		// negative! vision azimuths and gyro angles are opposites!
+		this.setSetpoint(-vision.update().getNearestTarget().getAzimuth());
 	}
 
 	@Override
 	protected void execute() {
 		cancelIfNoTarget();
-	}
-
-	@Override
-	protected boolean isFinished() {
-		if (Math.abs(this.getPIDController().getError()) <= Constants.visionTargetAzimuthThreshold) {
-			timeAtSetpoint += this.timeSinceInitialized() - lastCheck;
-		} else {
-			timeAtSetpoint = 0;
-		}
-		lastCheck = this.timeSinceInitialized();
-		return timeAtSetpoint >= Constants.pidSetpointWait;
-	}
-
-	@Override
-	protected void end() {
-		System.out.println("Vision Aligned Successfully");
-		drivetrain.tank(0, 0);
-	}
-
-	@Override
-	protected void interrupted() {
-		drivetrain.tank(0, 0);
-	}
-
-	@Override
-	protected double returnPIDInput() {
-		return vision.update().getNearestTarget().getAzimuth();
-	}
-
-	@Override
-	protected void usePIDOutput(double output) {
-		drivetrain.tank(output, -output);
+		super.execute();
 	}
 
 	private void cancelIfNoTarget() {
