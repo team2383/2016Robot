7c94006bc193cdfbcb66642024c25e67e5183c23
Matthew Alonso <=>
2016-04-01 08:52:03 -0400

    more vision I, reverted to direct camera feedback for first qual

--- src/com/team2383/robot/commands/VisionShoot.java
+++ src/com/team2383/robot/commands/VisionShoot.java
@@ -2,18 +2,8 @@ package com.team2383.robot.commands;
 
 import edu.wpi.first.wpilibj.command.CommandGroup;
 
-/**
- * Automatically spool, aim, and shoot
- *
- * TODO: Possibly do the gyro turn first, than use the old direct vision turn to
- * fine tune? maybe a slower than normal gyro turn
- * 
- * @author Matthew Alonso
- */
-
 public class VisionShoot extends CommandGroup {
 	public VisionShoot() {
-		addParallel(new SpoolToRPM());
 		addSequential(new VisionTurn());
 		addSequential(new UseVisionPreset());
 		addSequential(new AutoShoot());
--- src/com/team2383/robot/commands/VisionTurn.java
+++ src/com/team2383/robot/commands/VisionTurn.java
@@ -1,27 +1,73 @@
 package com.team2383.robot.commands;
 
+import static com.team2383.robot.HAL.drivetrain;
 import static com.team2383.robot.HAL.vision;
 
 import com.team2383.robot.Constants;
+import com.team2383.robot.subsystems.Drivetrain.Gear;
 
-public class VisionTurn extends GyroTurn {
+import edu.wpi.first.wpilibj.command.PIDCommand;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+
+public class VisionTurn extends PIDCommand {
+
+	private double timeAtSetpoint;
+	private double lastCheck;
 	private double timeWithoutTarget;
 
 	public VisionTurn() {
-		super(0);
+		super("VisionTurn", Constants.visionTurnP, Constants.visionTurnI, Constants.visionTurnD, .01);
+		requires(drivetrain);
+		requires(vision); // since PID loop runs faster, we require vision to
+							// get
+							// faster updates in the PID loop
+		this.getPIDController().setInputRange(-60.0, 60.0);
+		this.getPIDController().setOutputRange(-0.7, 0.7);
+		this.getPIDController().setSetpoint(0);
+		SmartDashboard.putData("VisionTurn Controller", this.getPIDController());
 	}
 
 	@Override
 	protected void initialize() {
-		super.initialize();
-		// negative! vision azimuths and gyro angles are opposites!
-		this.setSetpoint(-vision.update().getNearestTarget().getAzimuth());
+		drivetrain.enableBrake();
+		drivetrain.shiftTo(Gear.LOW);
 	}
 
 	@Override
 	protected void execute() {
 		cancelIfNoTarget();
-		super.execute();
+	}
+
+	@Override
+	protected boolean isFinished() {
+		if (Math.abs(this.getPIDController().getError()) <= Constants.visionTargetAzimuthThreshold) {
+			timeAtSetpoint += this.timeSinceInitialized() - lastCheck;
+		} else {
+			timeAtSetpoint = 0;
+		}
+		lastCheck = this.timeSinceInitialized();
+		return timeAtSetpoint >= Constants.pidSetpointWait;
+	}
+
+	@Override
+	protected void end() {
+		System.out.println("Vision Aligned Successfully");
+		drivetrain.tank(0, 0);
+	}
+
+	@Override
+	protected void interrupted() {
+		drivetrain.tank(0, 0);
+	}
+
+	@Override
+	protected double returnPIDInput() {
+		return vision.update().getNearestTarget().getAzimuth();
+	}
+
+	@Override
+	protected void usePIDOutput(double output) {
+		drivetrain.tank(output, -output);
 	}
 
 	private void cancelIfNoTarget() {
